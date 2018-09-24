f68c530a29f927938fa021b858822c4e2518d85c
Saeed khan <43543114+KSAEED2012@users.noreply.github.com>
2018-09-24 07:11:17 -0700

    Console.log() use in java script
    how to use Console.log() use in java script. that we define in this lecture.

---
+++ consol.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="js/script.js"></script>
+</head>
+<body>
+   <script>
+        var name = "Saeed Khan";
+        console.log(name);
+        var email = "khansaeed0005@yahoo.com";
+        console.log(email);
+        var age = 27;
+        console.log(age);
+        var data = true;
+        console.log(data);
+
+    
+   </script>
+</body>
+</html>
